ffd21706069fa983d8b8254647f05f84f28db4c2
msalad <msalad@uncc.edu>
2016-04-06 20:34:10 +0000

    Added some functionality to the links

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,10 +1,19 @@
 class ArticlesController < ApplicationController
     
-    def create
-        render plain: params[:article].inspect
-        byebug
+    private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+    
+    def index
+        @articles = Article.all
     end
     
     def new
     end
+    
+    def create
+        @article = Article.new(params[:article])
+    end
+    
 end
