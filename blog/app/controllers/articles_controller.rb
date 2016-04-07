d24905d7ce9181596330c0b982fa01965d25a55f
msalad <msalad@uncc.edu>
2016-04-07 04:02:59 +0000

    Added linking as well as create and show actions

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,6 +1,5 @@
 class ArticlesController < ApplicationController
     
-    private
   def article_params
     params.require(:article).permit(:title, :text)
   end
@@ -12,10 +11,25 @@ class ArticlesController < ApplicationController
     def new
     end
     
+    #The create is responsible for saving the article
     def create
         #Create an article
-        @article = Article.new(params[:article])
+        #@params holds the paramaters we are intrested in
+        @article = Article.new(article_params)
         
+       # byebug 
+        
+        #Save the article
+        @article.save
+        
+        #redirecting the user to the show action
+        redirect_to @article
+        
+        
+    end
+    
+    def show
+        @article = Article.find(params[:id])
     end
     
 end
